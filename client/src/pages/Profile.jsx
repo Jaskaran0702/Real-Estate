9da3f96d32199c134b13a1ae9978baec2faa83cf
Jaskaran0702 <21ucs097@lnmiit.ac.in>
2023-12-30 16:22:40 +0530

    create listing api done

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,11 +8,21 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase.js";
-import { SignOutUserFailure, SignOutUserStart, SignOutUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess, updateUserFailure,updateUserStart,updateUserSuccess } from "../redux/user/userSlice.js";
+import {
+  SignOutUserFailure,
+  SignOutUserStart,
+  SignOutUserSuccess,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser,loading,error } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setfile] = useState(undefined);
   const [fileperc, setfileperc] = useState(0);
   const [uploaderror, setuploaderror] = useState(false);
@@ -54,22 +64,22 @@ export default function Profile() {
     );
   };
   const handleChange = (e) => {
-    setformData({...formData, [e.target.id]: e.target.value});
+    setformData({ ...formData, [e.target.id]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       dispatch(updateUserStart());
-      const res = await fetch(`/api/user/update/${currentUser._id}`,{
-        method: 'POST',
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      if(data.success == false){
+      if (data.success == false) {
         dispatch(updateUserFailure(data.message));
         return;
       }
@@ -78,16 +88,16 @@ export default function Profile() {
     } catch (error) {
       dispatch(updateUserFailure(error.message));
     }
-  }
+  };
 
   const handleDeleteUser = async () => {
     try {
       dispatch(deleteUserStart());
-      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
-        method: 'DELETE',
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
       });
       const data = await res.json();
-      if(data.success===false){
+      if (data.success === false) {
         dispatch(deleteUserFailure(data.message));
         return;
       }
@@ -95,14 +105,14 @@ export default function Profile() {
     } catch (error) {
       dispatch(deleteUserFailure(error.message));
     }
-  }
+  };
 
   const handleSignOut = async () => {
     try {
       dispatch(SignOutUserStart());
-      const res = await fetch('/api/auth/signout');
+      const res = await fetch("/api/auth/signout");
       const data = await res.json();
-      if(data.success === false){
+      if (data.success === false) {
         dispatch(SignOutUserFailure(data.message));
         return;
       }
@@ -110,7 +120,7 @@ export default function Profile() {
     } catch (error) {
       dispatch(SignOutUserFailure(error.message));
     }
-  }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -130,13 +140,15 @@ export default function Profile() {
         />
         <p className="text-sm self-center">
           {uploaderror ? (
-            <span className="text-red-700">Error Image Upload(Image must be less than 2mb)</span>
+            <span className="text-red-700">
+              Error Image Upload(Image must be less than 2mb)
+            </span>
           ) : fileperc > 0 && fileperc < 100 ? (
             <span className="text-slate-700">{`Uploading ${fileperc}`}</span>
           ) : fileperc === 100 ? (
             <span className="text-green-700">Image succesfully uploaded</span>
           ) : (
-            ''
+            ""
           )}
         </p>
         <input
@@ -162,16 +174,28 @@ export default function Profile() {
           className="border p-3 rounded-lg"
           onChange={handleChange}
         />
-        <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
           {loading ? "Loading.." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
-        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign Out</span>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete Account
+        </span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign Out
+        </span>
       </div>
-      <p className="text-red-700 m-5">{error ? error : ''}</p>
-      <p className="text-green-700 m-5">{updateSuccess ? "Updated Successfully" : ''}</p>
+      <p className="text-red-700 m-5">{error ? error : ""}</p>
+      <p className="text-green-700 m-5">
+        {updateSuccess ? "Updated Successfully" : ""}
+      </p>
     </div>
   );
 }
